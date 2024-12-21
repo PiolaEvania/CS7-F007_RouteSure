eb4e5038ac561c74f989cd0cf7e89e2aeb8d1819
PiolaEvania <piolaevania@gmail.com>
2024-12-21 15:34:46 +0800

    Mengupdate beberapa code

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: 'https://app-route-sure.vercel.app',
+  origin: 'https://route-sure.vercel.app',
   method: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
